401d61af6f1936b6a5584562441162d558392244
Faiz <agustianto.d19@gmail.com>
2023-05-14 13:57:56 +0700

    change q-img to q-icon

--- src/pages/Menu.vue
+++ src/pages/Menu.vue
@@ -13,20 +13,20 @@
           </div>
           <div class="divider q-my-lg" />
           <div class="row items-center">
-            <q-img src="/icons/menu/orders.png" class="icon-menu" />
+            <q-icon name="img:/icons/menu/orders.svg" size="24px" />
             <p class="inter-sb text-base neutral-600 q-mb-none q-ml-md">Pemesanan Saya</p>
           </div>
           <div class="row items-center q-mt-md">
-            <q-img src="/icons/menu/promo.png" class="icon-menu" />
+            <q-icon name="img:/icons/menu/promo.svg" size="24px" />
             <p class="inter-sb text-base neutral-600 q-mb-none q-ml-md">Informasi Promo</p>
           </div>
           <div class="divider q-my-lg" />
           <div class="row items-center">
-            <q-img src="/icons/menu/settings.png" class="icon-menu" />
+            <q-icon name="img:/icons/menu/settings.svg" size="24px" />
             <p class="inter-sb text-base neutral-600 q-mb-none q-ml-md">Sunnting Akun</p>
           </div>
           <div @click="signOut()" class="row items-center q-mt-md cursor-pointer">
-            <q-img src="/icons/menu/logOut.png" class="icon-menu" />
+            <q-icon name="img:/icons/menu/logOut.svg" size="24px" />
             <p class="inter-sb text-base red-600 q-mb-none q-ml-md">Keluar</p>
           </div>
         </div>
